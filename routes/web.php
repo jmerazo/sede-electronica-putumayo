37e6894e2a34cf6ef8097e92ec409ed4a45c2e7c
jmerazo <jmerazo96@gmail.com>
2024-10-10 09:00:41 -0500

    fixed menu

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TransparenciaController;
+use App\Http\Controllers\MisionController;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/set-locale/{lang}', [App\Http\Controllers\LanguageController::class, 'setLocale'])->name('setLocale');
 
